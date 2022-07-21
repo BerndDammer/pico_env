8b9eda818b4200bf5a9fc51315702d54ab9e945a
pico4 <pico4@web.de>
2022-07-21 15:49:48 +0200

    Add Pak Generation

---
+++ pico_pak.bat
@@ -0,0 +1,5 @@
+rem bin\7z\7z -?
+
+bin\7z\7z a PicoPak05.zip bin doc links openocd preload_uf2 putty *.bat *.sh
+echo "....................."
+pause
---
+++ pico_pak_tar.sh
@@ -0,0 +1,3 @@
+tar -zcf PicoPak04.tar.gz bin doc links openocd preload_uf2 putty *.bat *.sh
+echo "....................."
+read
