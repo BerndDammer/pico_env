4d7df2b8af52422fc177d309e9762f0d3c0f7d47
Bernd Dammer <Bernd.Dammer@web.de>
2023-09-23 11:05:21 +0200

    feierabend

---
+++ run_putty_com8.bat
@@ -0,0 +1,3 @@
+rem bin\putty\putty
+rem pause
+start bin\putty\putty -serial COM8 -sercfg 115200,8,n,1,N
---
+++ run_putty_openocd_telnet_giselle.bat
@@ -0,0 +1,3 @@
+bin\putty\putty telnet://giselle.station43:4444
+pause ---------------------------
+
