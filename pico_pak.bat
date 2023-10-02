3d097364799660e420b4778579d93d519b8b0cca
Bernd Dammer <Bernd.Dammer@web.de>
2023-10-02 16:15:12 +0200

    pico pak

---
+++ pico_pak.bat
@@ -0,0 +1,5 @@
+rem bin\7z\7z -?
+
+bin\7z\7z a PicoPak_02_01.zip sdoc bin doc downloads FreeRTOS_Books links preload_uf2 pico-examples pico-extras pico-playground pico-project-generator pico-sdk ews_pico/clone_template.sh *.bat *.sh *.url *.cmd
+echo "....................."
+pause
